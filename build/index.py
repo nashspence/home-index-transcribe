7efa6ce7cf603b3d75a7346edbd9de77172980d6
Nash Spence <nashspence@gmail.com>
2024-10-05 23:24:39 -0700

    update index.py add example_module

--- build/index.py
+++ build/index.py
@@ -102,20 +102,18 @@ async def get_doc_count_meili():
         logging.exception(f"meili get stats failed")
         raise
 
-async def add_or_update_doc_meili(doc, wait_for_task=False):
+async def add_or_update_doc_meili(doc):
     if not index:
         raise Exception("meili index did not init")
     
     if doc:
         try:
-            task = await index.update_documents([doc])
-            if wait_for_task:
-                await wait_for_task_completion_meili(task)
+            await index.update_documents([doc])
         except Exception:
             logging.exception(f"meili update documents failed")
             raise
 
-async def add_or_update_docs_meili(docs, wait_for_task=False):
+async def add_or_update_docs_meili(docs):
     if not index:
         raise Exception("meili index did not init")
     
@@ -123,14 +121,12 @@ async def add_or_update_docs_meili(docs, wait_for_task=False):
         try:
             for i in range(0, len(docs), MEILISEARCH_BATCH_SIZE):
                 batch = docs[i:i + MEILISEARCH_BATCH_SIZE]
-                task = await index.update_documents(batch)
-                if wait_for_task:
-                    await wait_for_task_completion_meili(task)
+                await index.update_documents(batch)
         except Exception:
             logging.exception(f"meili update documents failed")
             raise
 
-async def delete_docs_by_id_meili(ids, wait_for_task=False):
+async def delete_docs_by_id_meili(ids):
     if not index:
         raise Exception("meili index did not init")
     
@@ -138,9 +134,7 @@ async def delete_docs_by_id_meili(ids, wait_for_task=False):
         if ids:
             for i in range(0, len(ids), MEILISEARCH_BATCH_SIZE):
                 batch = ids[i:i + MEILISEARCH_BATCH_SIZE]
-                task = await index.delete_documents(ids=batch)
-                if wait_for_task:
-                    await wait_for_task_completion_meili(task)
+                await index.delete_documents(ids=batch)
     except Exception:
         logging.exception(f"meili delete documents failed")
         raise
@@ -202,20 +196,19 @@ async def get_all_pending_jobs(module):
         logging.exception(f"meili get documents failed")
         raise
 
-async def wait_for_task_completion_meili(task_info):
+async def wait_for_idle_meili():
     if not client:
         raise Exception("meili index did not init")
     
     try:
         while True:
-            task = await client.get_task(task_info.task_uid)
-            if task.status == 'succeeded':
-                return
-            elif task.status == 'failed':
-                raise Exception(f"meili task failed {str(task_info)}")
-            await asyncio.sleep(0.5)
+            tasks = await client.get_tasks()
+            active_tasks = [task for task in tasks.results if task.status in ['enqueued', 'processing']]
+            if len(active_tasks) == 0:
+                break
+            await asyncio.sleep(1)
     except Exception:
-        logging.exception(f"meili get task failed")
+        logging.exception(f"meili wait for idle failed")
         raise
 
 def get_mime_magic(file_path):
@@ -249,74 +242,69 @@ def get_relative_path_from_file_path(file_path):
     return Path(file_path).relative_to(OS_DIRECTORY_TO_INDEX).as_posix()
 
 async def sync_meili_docs():
-    all_docs = await get_all_docs_meili()
-    existing_meili_file_paths = set()
-    existing_docs = {}
+    expected_documents = await get_all_docs_meili()
+    expected_documents_by_id = {doc["id"]: doc for doc in expected_documents}
+    previous_file_paths = {get_file_path_from_meili_doc(doc) for doc in expected_documents}
+    current_file_paths = set()
+    added_document_ids = set()
+    updated_document_ids = set()
+    create_document_tasks = []
+
+    logging.info(f"meili {len(expected_documents)} indexed")
     
-    logging.info(f"meili {len(all_docs)} indexed")
+    doc_creation_semaphore = asyncio.Semaphore(32)
+    async def async_create_meili_doc_from_file_path(file_path):
+        async with doc_creation_semaphore:
+            return await asyncio.to_thread(create_meili_doc_from_file_path, file_path)
 
-    for doc in all_docs:
-        file_path = get_file_path_from_meili_doc(doc)
-        existing_meili_file_paths.add(file_path)
-        existing_docs[doc["id"]] = doc
+    directory_stack = [OS_DIRECTORY_TO_INDEX]
 
-    directories_to_scan = [OS_DIRECTORY_TO_INDEX]
-    updated = []
-    exists = set()
+    while directory_stack:
+        dir_path = directory_stack.pop()
+        try:
+            entries = await asyncio.to_thread(lambda: list(os.scandir(dir_path)))
+            for entry in entries:
+                if entry.is_dir(follow_symlinks=False):
+                    directory_stack.append(entry.path)
+                elif entry.is_file(follow_symlinks=False):
+                    id = get_meili_id_from_file_path(entry.path)
+                    path_mtime = Path(entry.path).stat().st_mtime
+                    if id not in expected_documents_by_id or expected_documents_by_id[id]["mtime"] < path_mtime:
+                        task = asyncio.create_task(async_create_meili_doc_from_file_path(entry.path))
+                        create_document_tasks.append(task)
+                        if id not in expected_documents_by_id:
+                            added_document_ids.add(id)
+                        elif expected_documents_by_id[id]["mtime"] < path_mtime:
+                            updated_document_ids.add(id)
+                    current_file_paths.add(entry.path)
+        except Exception:
+            logging.exception(f'os scan directory failed "{dir_path}"')
 
-    with ThreadPoolExecutor(max_workers=32) as executor:
-        futures = []
+    done, _ = await asyncio.wait(create_document_tasks)
+    added_or_updated_documents = [task.result() for task in done if task.result() is not None]
 
-        while directories_to_scan:
-            dir_path = directories_to_scan.pop()
-            try:
-                with os.scandir(dir_path) as it:
-                    for entry in it:
-                        if entry.is_dir(follow_symlinks=False):
-                            directories_to_scan.append(entry.path)
-                        elif entry.is_file(follow_symlinks=False):
-                            futures.append(executor.submit(create_meili_doc_from_file_path, entry.path, existing_docs))
-            except Exception:
-                logging.exception(f'os scan directory failed "{dir_path}"')
+    deleted_file_paths = previous_file_paths - current_file_paths
+    deleted_document_ids = {get_meili_id_from_file_path(fp) for fp in deleted_file_paths}
 
-        for future in as_completed(futures):
-            result = future.result()
-            if result:
-                file_path, document, status = result
-                updated.append(document)
-                if status != "modified":
-                    exists.add(file_path)
+    await delete_docs_by_id_meili(list(deleted_document_ids | updated_document_ids))
+    await wait_for_idle_meili()
+    await add_or_update_docs_meili(added_or_updated_documents)
+    await wait_for_idle_meili()
 
-    deleted = existing_meili_file_paths - exists
-    deleted_meili_ids = [get_meili_id_from_file_path(file_path) for file_path in deleted]
+    total_expected_documents = await get_doc_count_meili()
+    logging.info(f"meili added {len(added_document_ids)}")
+    logging.info(f"meili updated {len(updated_document_ids)}")
+    logging.info(f"meili deleted {len(deleted_document_ids)}")
+    logging.info(f"meili {total_expected_documents} now indexed")
 
-    await delete_docs_by_id_meili(deleted_meili_ids, wait_for_task=True),
-    await add_or_update_docs_meili(updated, wait_for_task=True)
-    
-    doc_count = await get_doc_count_meili()
-    
-    logging.info(f"meili deleted {len(deleted_meili_ids)} docs")
-    logging.info(f"meili created/updated {len(updated)}")
-    logging.info(f"meili {doc_count} docs now indexed")
-
-def create_meili_doc_from_file_path(file_path, existing_docs = {}):
+def create_meili_doc_from_file_path(file_path):
     try:
         path = Path(file_path)
         relative_path = path.relative_to(OS_DIRECTORY_TO_INDEX).as_posix()
         stat = path.stat()
-
         current_mtime = stat.st_mtime
-
         mime = get_mime_magic(path)
         doc_id = get_meili_id_from_relative_path(relative_path)
-        
-        status = "new"    
-        if doc_id in existing_docs:
-            if existing_docs[doc_id]["mtime"] < current_mtime:
-                status = "modified"
-            else:
-                status = "same"
-
         document = {
             "id": doc_id,
             "name": path.name,
@@ -326,13 +314,10 @@ def create_meili_doc_from_file_path(file_path, existing_docs = {}):
             "url": get_url_from_relative_path(relative_path),
             "type": mime,
         }
-
         for module in modules:
             if module.does_support_mime(mime):
-                if status == "modified" or status == "new":
-                    document[module.FIELD_NAME] = 0
-
-        return (file_path, document, status)
+                document[module.FIELD_NAME] = 0
+        return document
     except Exception:
         logging.exception(f'os create doc failed "{file_path}"')
         return None
@@ -523,6 +508,7 @@ async def update_meili_docs():
         for module in modules:
             await augment_meili_docs(module)
         logging.info(f'wait for next cycle')
+        await wait_for_idle_meili()
         await asyncio.sleep(SLEEP_BETWEEN_MODULE_RUNS)
            
 async def main():
---
+++ build/modules/example_module.py
@@ -0,0 +1,89 @@
+"""
+example.py - Template for Creating a New Module in Directory Indexing App
+
+This module serves as a comprehensive template for developing new modules
+for the directory indexing application. It outlines the necessary structure,
+functions, and documentation required to integrate seamlessly with the app.
+
+"""
+
+# Define supported image MIME types
+SUPPORTED_MIMES = {
+    'image/jpeg',
+    'image/png',
+    'image/gif',
+    'image/tiff',
+    'image/bmp',
+    'image/webp'
+}
+
+# Module Metadata Constants
+NAME = "image_metadata"                    # REQUIRED: Unique name of the module
+FIELD_NAME = "imageMetadataVersion"        # REQUIRED: Field name for versioning
+DATA_FIELD_NAMES = []                      # REQUIRED: Data fields to fetch for continue incrementally
+MAX_WORKERS = 8                            # REQUIRED: Maximum concurrent workers
+VERSION = 1                                # REQUIRED: Module version, will cause all match files to process again if incremented
+
+async def init():
+    """
+    REQUIRED
+    
+    Initialize the module.
+
+    This asynchronous function is called when the module is loaded.
+    It can be used to set up resources, load models, or perform initial
+    configurations required by the module.
+
+
+    Returns:
+        None
+    """
+    return
+
+async def cleanup():
+    """
+    REQUIRED
+    
+    Clean up resources used by the module.
+
+    This asynchronous function is called when the module is unloaded.
+    It should handle the release of any resources or perform necessary
+    cleanup tasks to ensure no residual effects remain.
+
+    Returns:
+        None
+    """
+    return
+
+def does_support_mime(mime):
+    """
+    REQUIRED
+    
+    Determine if the module supports a given MIME type.
+
+    Args:
+        mime (str): The MIME type of the file to check.
+
+    Returns:
+        bool: True if the MIME type is supported, False otherwise.
+    """
+    return mime in SUPPORTED_MIMES
+
+async def get_fields(file_path, doc):
+    """
+    REQUIRED
+    
+    Extract metadata fields from the file
+
+    This asynchronous generator function processes the file located
+    at `file_path`, extracts its metadata fields, and yields each field
+    as a separate dictionary entry. Designed to handle incremental data
+    building by yielding partial results.
+
+    Args:
+        file_path (str): The full path to the file.
+        doc (dict): The existing document dictionary that may contain pre-existing data to continue a previous run.
+
+    Yields:
+        dict: A dictionary with extracted metadata fields.
+    """
