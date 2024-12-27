87a1c0a32bfc0834622a949806fdc0f0ca5b124e
Nash Spence <nashspence@gmail.com>
2024-12-27 16:29:49 +0000

    make docker-compose more flexible

--- .devcontainer/poststart.sh
+++ .devcontainer/poststart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 /usr/local/bin/start-docker.sh
-if [ -d /home/ns/.ssh ]; then
-  cp -r /home/ns/.ssh /root/
+if [ -d /tmp/.ssh ]; then
+  cp -r /tmp/.ssh /root/
   chmod 700 /root/.ssh
   chmod 600 /root/.ssh/*
 fi
